8e7d9942542db462f8a1a91a72eaa6db6268cbaf
kana <nicht@s8.xrea.com>
2011-06-26 23:48:11 +0900

    Helper API: Add scheme->ivs

--- t/helper-api.t
+++ t/helper-api.t
@@ -9,6 +9,51 @@
 
 
 
+(describe "convert-identifier-conventions"
+  (it "should return the same spell if ordinary identifier is given"
+    (expect (convert-identifier-conventions "foo") equal? "foo")
+    (expect (convert-identifier-conventions "bar123") equal? "bar123")
+    (expect (convert-identifier-conventions "s:local") equal? "s:local")
+    )
+  (it "should convert 'x?' convention"
+    (expect (convert-identifier-conventions "foo?") equal? "foo_p")
+    (expect (convert-identifier-conventions "foo?bar") equal? "foo?bar")
+    )
+  (it "should convert 'x!' convention"
+    (expect (convert-identifier-conventions "foo!") equal? "foo_x")
+    (expect (convert-identifier-conventions "foo!bar") equal? "foo!bar")
+    )
+  (it "should convert 'x->y' convention"
+    (expect (convert-identifier-conventions "foo->bar") equal? "foo_to_bar")
+    (expect (convert-identifier-conventions "x->y->z") equal? "x_to_y_to_z")
+    )
+  (it "should convert 'x-y-z' convention"
+    (expect (convert-identifier-conventions "x-y-z") equal? "x_y_z")
+    (expect (convert-identifier-conventions "strange-") equal? "strange_")
+    )
+  (it "should convert '%x' convention"
+    (expect (convert-identifier-conventions "%internal") equal? "_internal")
+    (expect (convert-identifier-conventions "w%i%r%e%d") equal? "w_i_r_e_d")
+    )
+  )
+
+(describe "convert-regexp-conventions"
+  (it "should convert given Scheme regexp into Vim script string"
+    (expect (convert-regexp-conventions #/foo/) equal? "'foo'")
+    (expect (convert-regexp-conventions #/\(x\|y\)/) equal? "'\\(x\\|y\\)'")
+    (expect (convert-regexp-conventions #/'/) equal? "''''")
+    )
+  )
+
+(describe "convert-string-conventions"
+  (it "should convert given Scheme string into Vim script string"
+    (expect (convert-string-conventions "foo") equal? "\"foo\"")
+    (expect (convert-string-conventions "f\"oo") equal? "\"f\\\"oo\"")
+    (expect (convert-string-conventions "f'oo") equal? "\"f'oo\"")
+    (expect (convert-string-conventions "-\x1f-") equal? "\"-\\x1f-\"")
+    )
+  )
+
 (describe "=ex="
   (it "should insert an empty list into the first element of new list"
     (expect (=ex= 1 2 3) equal? '(() 1 "\n" 2 "\n" 3 "\n"))
@@ -25,6 +70,39 @@
               (if " " (foo == bar)) "\n"
               (echo " " 1 " " 2) "\n"
               endif "\n"))
+    )
+  )
+
+(describe "scheme->ivs"
+  (it "should convert given Scheme boolean into equivalent one in Vim script"
+    (expect (scheme->ivs #f) equal? 0)
+    (expect (scheme->ivs #t) equal? 1)
+    )
+  (it "should convert given Scheme number into equivalent one in Vim script"
+    (expect (scheme->ivs 0) equal? 0)
+    (expect (scheme->ivs 123) equal? 123)
+    (expect (scheme->ivs -123) equal? -123)
+    (expect (scheme->ivs (- (ash 1 31) 1)) equal? (- (ash 1 31) 1))
+    (expect (scheme->ivs (ash -1 31)) equal? (ash -1 31))
+    (expect (scheme->ivs (ash 1 31)) raise?)
+    (expect (scheme->ivs (ash -1 32)) raise?)
+    (expect (scheme->ivs 0.123) raise?)
+    (expect (scheme->ivs #i123) raise?)
+    (expect (scheme->ivs 1+2i) raise?)
+    )
+  (it "should convert given Scheme regexp into equivalent one in Vim script"
+    (expect (scheme->ivs #/\(foo\|bar\)/) equal? "'\\(foo\\|bar\\)'")
+    )
+  (it "should convert given Scheme string into equivalent one in Vim script"
+    (expect (scheme->ivs "f\"oo") equal? "\"f\\\"oo\"")
+    )
+  (it "should convert given Scheme symbol into equivalent one in Vim script"
+    (expect (scheme->ivs 'foo!) equal? "foo_x")
+    )
+  (it "should fail for other Scheme objects"
+    (expect (scheme->ivs '()) raise?)
+    (expect (scheme->ivs '(x y z)) raise?)
+    (expect (scheme->ivs (lambda () '())) raise?)
     )
   )
 
--- vim/xire.scm
+++ vim/xire.scm
@@ -2,8 +2,12 @@
   (export
     ; Public API
     =ex=
+    scheme->ivs
 
     ; Not public, but exported to test.
+    convert-identifier-conventions
+    convert-regexp-conventions
+    convert-string-conventions
     ))
 (select-module vim.xire)
 
@@ -15,6 +19,91 @@
 
 ;;; Helper API
 ;;; ==========
+
+;; Convert identifier conventions between Scheme and Vim script.  For example,
+;; Scheme usually uses hyphen (-) to join words in an identifier, while Vim
+;; script uses underscore (_) for that purpose.  This procedure converts such
+;; conventions.
+(define (convert-identifier-conventions scheme-identifier-string)
+  ; FIXME: Raise error for invalid characters as Vim script identifier.
+  (regexp-replace-all* scheme-identifier-string
+                       #/\?$/ "_p"
+                       #/!$/ "_x"
+                       #/->/ "_to_"
+                       #/[-%]/ "_"
+                       ))
+
+;; Convert Scheme regexp object into equivalent one in Vim script.
+;; Unlike convert-string-conventions, there is no limitation to write regexp.
+;; Because external representation of regexp in Scheme is almost same as one
+;; in Vim script.
+(define (convert-regexp-conventions scheme-regexp)
+  (regexp-replace-all* (regexp->string scheme-regexp)
+                       #/'/ "''"
+                       #/^(.*)$/ "'\\1'"
+                       ))
+
+;; Convert Scheme string object into Vim script string notation.
+;;
+;; In xire script, Vim script strings are written as Scheme strings.  But both
+;; notations are not completely equivalent.  So that we put the following
+;; limitations on Scheme strings which will be compiled into Vim script:
+;;
+;; (i) In Scheme strings, only the following backslash-escape notations may be
+;;     used:
+;;
+;;     \\ \" \f \n \r \t \uNNNN \xNN \<whitespace>*<newline><whitespace>*
+;;
+;;     - All but the last notation are the same as Vim script's ones, so that
+;;       external representation of Scheme strings and Vim script strings are
+;;       the same if this condition is met.  Therefore we can simply write
+;;       Scheme strings as if they are Vim script strings.
+;;
+;;     - The last notation is processed by Scheme's reader and it is simply
+;;       ignored.  So that it may be used too.
+;;
+;;     - Other notations (\0, \UNNNNNNNN) must not be used.  Because:
+;;       - There is no equivalent for \UNNNNNNNN.
+;;       - Vim script cannot handle NUL character as is.  Though we can write
+;;         "\0" in Vim script, such strings are essentially wrong.  So that it
+;;         must not be used.
+;;
+;; (ii) The following backslash-escape notations in Vim script are not
+;;      available in xire script:
+;;
+;;      (o) \. \.. \... (arbitrary byte, in octal digits)
+;;      (x) \x.         (arbitrary byte, in single hex digit)
+;;      (X) \X. \X..    (equivalent to \x. and \x..)
+;;      (U) \U....      (equivalent to \u....)
+;;      (b) \b          (equivalent to \<BS>)
+;;      (e) \e          (equivalent to \<Esc>)
+;;      (k) \<Key>      (special key sequence)
+;;
+;;      - (o), (x) and (X): Use "\xNN" instead.
+;;      - (b) and (e): Use "\xNN" instead.
+;;      - (U): Incompatible with Gauche's "\UNNNNNNNN" notation and it is
+;;        rarely used.
+;;      - (k): Use (kbd "<Key> ...") form instead.  [KEYSEQ]
+;;
+;; [KEYSEQ]
+;;     In Vim script, it's necessary to write special key sequences in
+;;     the following situations:
+;;
+;;     (A) To denote input:
+;;         (a) :execute 'normal!' "\<Key>"
+;;         (b) :call feedkeys("\<Key>", 't')
+;;         (c) :map <expr> {lhs} "\<Key>"
+;;         (d) :map {lhs} <C-r>="\<Key>"<Return>
+;;     (B) To compare input:
+;;         (a) visualmode() ==# "\<C-v>"
+;;         (b) 0 <= index(["\<C-c>", "\<Esc>"], nr2char(getchar()))
+;;
+;;     But it's possible to write equivalent Vim script if (kbd "<Key> ...")
+;;     form is available.  So that it's not a problem even if we cannot write
+;;     "\<Key>" directly in xire script.
+(define (convert-string-conventions scheme-string)
+  ; FIXME: Check the above limitations are met.
+  (format "~s" scheme-string))
 
 (define (=ex= . ex-cmd-ivss)
   (define (insert-newline xs)
@@ -32,6 +121,43 @@
         (insert-newline
           (insert-space
             ex-cmd-ivss))))
+  )
+
+(define (scheme->ivs x)
+  ; Vim script numbers are 32-bit signed integers.  (cf. :help Number)
+  (define MINIMAL-NUMBER (ash -1 31))
+  (define MAXIMUM-NUMBER (- (ash 1 31) 1))
+
+  (cond
+    [(boolean? x)
+     (if x
+       1
+       0)]
+    [(number? x)
+     (cond
+       [(< x MINIMAL-NUMBER)
+        (errorf "Too small number for Vim script: ~s" x)]
+       [(< MAXIMUM-NUMBER x)
+        (errorf "Too big number for Vim script: ~s" x)]
+       [(inexact? x)
+        (errorf "Inexact number can not be expressed in Vim script: ~s" x)]
+       [(integer? x)
+        x]
+       ; FIXME: Support floating point numbers in Vim script.
+       [else
+         (errorf "Invalid number for Vim script: ~s" x)])]
+    [(regexp? x)
+     (convert-regexp-conventions x)]
+    [(string? x)
+     (convert-string-conventions x)]
+    [(symbol? x)
+     (convert-identifier-conventions (symbol->string x))]
+    ; List literal is provided by a xire macro.
+    ; Dictionary literal is provided by a xire macro.
+    ; FIXME: How about Funcref?
+    [else
+      (errorf "Invalid Scheme object for Vim script: ~s" x)]
+    )
   )
 
 
